4bda7666a31732d3f4dcec778d4ad2bdbe463ab5
Jayalekha Ramesh <jramesh@temenos.com>
2020-10-27 12:52:16 +1100

    Update sql to enable verification

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 
-INSERT INTO Person (name, phone_number, email_address) VALUES
-    ('Alex', 0412345678, 'alex@abc.com'),
-    ('Bob', 0412345679, 'bob@abc.com'),
-    ('Cyril', 0470345679, 'cyril@abc.com');
+INSERT INTO Person (id, name, phone_number, email_address) VALUES
+    (1, 'Alex', 0412345678, 'alex@abc.com'),
+    (2, 'Bob', 0412345679, 'bob@abc.com'),
+    (3, 'Cyril', 0470345679, 'cyril@abc.com');
 
-INSERT INTO Book (title, author, isbn, publication, person_id) VALUES
-('The Wings of Fire', 'Abdul Kalaam', 91234876108424, 'Oxford', [1,3]),
-('The Lost Symbol', 'Dan Brown', 91234872308424, 'Oxford', [1]),
-('False Impression', 'Jeffery Archer', 91564876108412, 'Oxford', [2,3]);
+INSERT INTO Book (id, title, author, isbn, publication, person_id) VALUES
+(1, 'The Wings of Fire', 'Abdul Kalaam', 91234876108424, 'Oxford', 1),
+(2, 'The Lost Symbol', 'Dan Brown', 91234872308424, 'Oxford', 3),
+(3,  'False Impression', 'Jeffery Archer', 91564876108412, 'Oxford', 2);
